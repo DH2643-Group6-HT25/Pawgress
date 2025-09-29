feb60441221288f2997b4fba386b355bad8eff44
SaraMolin <sarakarinmolin@gmail.com>
2025-09-29 23:10:12 +0200

    Example on how to use styled-components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,12 @@ import reactLogo from './assets/react.svg';  // Reserved for future use??
 import './App.css';
 import { socket } from './socket';
 
+import { ThemeProvider } from "styled-components";
+import { theme } from "./views/components/theme";
+import { MyButton } from './views/components/MyButton';
+import { MenuCard } from './views/components/MenuCard';
+
+
 function App() {
   const [count, setCount] = useState(0);
 
@@ -19,7 +25,20 @@ function App() {
     };
   }, []);
 
-  return < div/ >;
+  
+  return (
+    <div>
+      <ThemeProvider theme={theme}>
+        <MyButton primary>Test styled theme button Primär</MyButton>
+        <MyButton>Default</MyButton>
+      </ThemeProvider>
+
+      <ThemeProvider theme={theme}>
+        <MenuCard></MenuCard>
+      </ThemeProvider> 
+    </div>
+
+  );
 }
 
 export default App;
