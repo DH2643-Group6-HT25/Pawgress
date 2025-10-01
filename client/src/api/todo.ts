7f1307a089fe56d1d58403efab2fa57be9e5b496
klarakarlssoon <klakar@kth.se>
2025-10-01 19:08:59 +0200

    feat(todo): uppdatera client/src/api/todo.ts (create/list/delete via API)

--- client/src/api/todo.ts
+++ client/src/api/todo.ts
@@ -4,15 +4,15 @@ export async function getTodos(userId: string) {
 }
 
 export async function addTodo(todo: string, user: string) {
-  const res = await fetch('/todo', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
+  const res = await fetch("/todo", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ todo, user }),
   });
   return res.json();
 }
 
 export async function deleteTodo(id: string) {
-  const res = await fetch(`/todo/${id}`, { method: 'DELETE' });
+  const res = await fetch(`/todo/${id}`, { method: "DELETE" });
   return res.json();
 }
