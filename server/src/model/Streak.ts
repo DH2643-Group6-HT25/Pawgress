b427904035704786b2b9cd87b51a5f1518cda289
Jingyi Yan <jingyiy@kth.se>
2025-10-01 22:52:54 +0200

    update streak schema

---
+++ server/src/model/Streak.ts
@@ -0,0 +1,33 @@
+import { Schema, model, Types } from "mongoose";
+
+// Raw document interface. Contains the data type as it will be stored
+// in MongoDB. So you can ObjectId, Buffer, and other custom primitive data types.
+// But no Mongoose document arrays or subdocuments.
+export interface IStreakHistory {
+  date: Date;
+  todoNumber: number;
+}
+
+export interface IStreak {
+  currentStreak: number;
+  bestStreak: number;
+  history: IStreakHistory[];
+  user: Types.ObjectId;
+}
+
+// Schema
+const StreakHistoryschema = new Schema<IStreakHistory>({
+  date: { type: Date, required: true },
+  todoNumber: { type: Number, default: 0 },
+});
+
+const Streakschema = new Schema<IStreak>({
+  currentStreak: { type: Number, default: 0 },
+  bestStreak: { type: Number, default: 0 },
+  history: [StreakHistoryschema],
+  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+});
+
+const StreakModel = model("IStreak", Streakschema);
+
+export default StreakModel;
--- server/src/model/Users.ts
+++ server/src/model/Users.ts
@@ -1,4 +1,4 @@
-import { Schema, model, Types } from 'mongoose';
+import { Schema, model, Types } from "mongoose";
 
 // Raw document interface. Contains the data type as it will be stored
 // in MongoDB. So you can ObjectId, Buffer, and other custom primitive data types.
@@ -6,35 +6,41 @@ import { Schema, model, Types } from 'mongoose';
 export interface IUser {
   name: string;
   email: string;
-  passwordHash: string, 
+  passwordHash: string;
   pet: Types.ObjectId;
   todo: Types.ObjectId[];
   dailyJournal: Types.ObjectId[];
-  strak?: number;
+  streak: Types.ObjectId[];
   food: number;
 }
 
 // Schema
 const Userschema = new Schema<IUser>({
-  name: { type: String, required: true, unique: true, trim: true},
-  email: { type: String, required: true, unique: true, trim: true, lowercase: true, 
+  name: { type: String, required: true, unique: true, trim: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
     validate: {
       validator: function (value) {
         return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
       },
-      message: 'Invalid email address format',
-    }, },
-  passwordHash:{ type: String, required: true},
-  pet: { type: Schema.Types.ObjectId, ref: 'Pet' },
-  todo: [{ type: Schema.Types.ObjectId, ref: 'ITodoList' }],
-  dailyJournal: [{ type: Schema.Types.ObjectId, ref: 'IDailyJournal' }],
-  strak: {type: Number, required: false, default: 0},
-  food: {type: Number, required: true, default: 0},
+      message: "Invalid email address format",
+    },
+  },
+  passwordHash: { type: String, required: true },
+  pet: { type: Schema.Types.ObjectId, ref: "Pet" },
+  todo: [{ type: Schema.Types.ObjectId, ref: "ITodoList" }],
+  dailyJournal: [{ type: Schema.Types.ObjectId, ref: "IDailyJournal" }],
+  streak: [{ type: Schema.Types.ObjectId, ref: "IStreak" }],
+  food: { type: Number, required: true, default: 0 },
 });
 
-const UserModel = model('User', Userschema);
+const UserModel = model("User", Userschema);
 
-const doc = new UserModel({ name: 'test', email: 'test' });
+const doc = new UserModel({ name: "test", email: "test" });
 
 console.log(doc.name); // string
 console.log(doc.email); // string
