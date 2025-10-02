7e265a60928b0682e4b7c935114bca0f0ac0e65e
SaraMolin <sarakarinmolin@gmail.com>
2025-10-02 14:29:31 +0200

    journal api added

--- server/src/model/DailyJournals.ts
+++ server/src/model/DailyJournals.ts
@@ -3,8 +3,16 @@ import { Schema, model, Types } from 'mongoose';
 // Raw document interface. Contains the data type as it will be stored
 // in MongoDB. So you can ObjectId, Buffer, and other custom primitive data types.
 // But no Mongoose document arrays or subdocuments.
+
+export interface IFormatting {
+  start: number;
+  end: number;
+  type: "bold" | "italic" | "underline";
+}
+
 export interface IDailyJournal {
   journal: string;
+  formatting?: IFormatting[];
   imageUrl?: string; // store the img to the cloud?
   date: Date;
   user: Types.ObjectId;
