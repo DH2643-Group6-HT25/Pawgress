9824078d7ed68bf556c36903731b7f823b71e232
Jingyi Yan <jingyiy@kth.se>
2025-10-07 18:47:22 +0200

    suspenseView with anim

--- client/src/views/WelcomePageView.tsx
+++ client/src/views/WelcomePageView.tsx
@@ -1,8 +1,8 @@
-import { Link } from 'react-router'
-import styled from 'styled-components'
+import { Link } from "react-router";
+import styled from "styled-components";
 
-import { MyButton } from '../components/MyButton'
-import Header from '../components/Header'
+import { MyButton } from "../components/MyButton";
+import Header from "../components/Header";
 
 function WelcomePage() {
   return (
@@ -18,10 +18,10 @@ function WelcomePage() {
 
         <ButtonRow>
           {/* Use Links so routing works */}
-          <Primary as={Link} to='/signup'>
+          <Primary as={Link} to="/signup">
             Sign Up
           </Primary>
-          <Secondary as={Link} to='/login'>
+          <Secondary as={Link} to="/login">
             Log In
           </Secondary>
         </ButtonRow>
@@ -31,18 +31,21 @@ function WelcomePage() {
       {/* <BgIcon className="cat" src="/icons/cat.png" alt="" /> */}
 
       <div>
-        <MyButton primary as={Link} to='/dashboard'>
+        <MyButton primary as={Link} to="/dashboard">
           Dashboard
         </MyButton>
-        <MyButton primary as={Link} to='/loading'>
+        <MyButton primary as={Link} to="/loading">
           Loading Page
+        </MyButton>
+        <MyButton primary as={Link} to="/onsuspense">
+          Suspense View
         </MyButton>
       </div>
     </Wrapper>
-  )
+  );
 }
 
-export default WelcomePage
+export default WelcomePage;
 
 /* ---------- styles ---------- */
 const Wrapper = styled.div`
@@ -51,13 +54,13 @@ const Wrapper = styled.div`
   display: grid;
   place-items: center;
   background: linear-gradient(180deg, #f4f1df 0%, #cddcd8 100%);
-`
+`;
 
 const Content = styled.div`
   text-align: center;
   max-width: 720px;
   padding: 0 24px;
-`
+`;
 
 const Headline = styled.h1`
   font-size: 48px;
@@ -65,19 +68,19 @@ const Headline = styled.h1`
   font-weight: 800;
   letter-spacing: -0.02em;
   margin: 0 0 16px 0;
-`
+`;
 
 const SubHeadline = styled.p`
   font-size: 20px;
   line-height: 1.5;
   opacity: 0.85;
   margin: 0 0 28px 0;
-`
+`;
 
 const ButtonRow = styled.div`
   display: inline-flex;
   gap: 12px;
-`
+`;
 
 const BaseBtn = styled.button`
   padding: 14px 24px;
@@ -87,17 +90,17 @@ const BaseBtn = styled.button`
   border: 0;
   cursor: pointer;
   text-decoration: none;
-`
+`;
 
 const Primary = styled(BaseBtn)`
   background: #111;
   color: #fff;
-`
+`;
 
 const Secondary = styled(BaseBtn)`
   background: #444;
   color: #fff;
   opacity: 0.95;
-`
+`;
 
 // const BgIcon = styled.img` ... optional absolute positions ... `;
