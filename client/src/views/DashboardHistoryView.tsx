5d76ecd95d8062a382651e300b1a7f13a4faa058
SaraMolin <sarakarinmolin@gmail.com>
2025-10-11 19:01:18 +0200

    Journal formik

--- client/src/views/DashboardHistoryView.tsx
+++ client/src/views/DashboardHistoryView.tsx
@@ -1,9 +1,19 @@
+import { InsideCard, InsideCardText } from '../components/CardComponents'
 import { MenuCard } from '../components/MenuCard'
 
 function DashboardHistoryView() {
   return (
     <MenuCard title='History' isUsingCloseButton linkCloseButton='/dashboard'>
-      <div>This is history</div>
+      <InsideCard primary>
+        <InsideCardText>
+          Here you can view your past activities and track your progress over time. Review your achievements and milestones to stay motivated on your journey!
+        </InsideCardText>        
+      </InsideCard>
+      <InsideCard>
+        <InsideCardText>
+          Here you can view your past activities and track your progress over time. Review your achievements and milestones to stay motivated on your journey!
+        </InsideCardText>
+      </InsideCard>
     </MenuCard>
   )
 }
