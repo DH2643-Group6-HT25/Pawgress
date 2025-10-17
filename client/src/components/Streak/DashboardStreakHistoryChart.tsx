d7281dbd5f43739231d62571721d1b2def9257b8
Jingyi Yan <jingyiy@kth.se>
2025-10-17 16:05:10 +0200

    move chart to folder

--- client/src/components/Streak/DashboardStreakHistoryChart.tsx
+++ client/src/components/Streak/DashboardStreakHistoryChart.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from 'react'
 import * as d3 from 'd3'
-import type { StreakHistoryObject } from '../models/streak/streakType'
+import type { StreakHistoryObject } from '../../models/streak/streakType'
 
 interface PropTypes {
   streakHistory: StreakHistoryObject[]
