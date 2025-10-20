eaf255411f603a20e6a76c22c4e90288cda3e72a
diagnosa <diagnosa@kth.se>
2025-10-20 02:56:29 +0200

    init deploy to azure

--- client/src/api/config.ts
+++ client/src/api/config.ts
@@ -1,6 +1,2 @@
 export const INTERNAL_API_URL: string =
-  import.meta.env.VITE_SERVER_URL || "http://localhost:3001";
-
-export const EXTERNAL_API_URL: string =
-  import.meta.env.VITE_API_URL ||
-  "https://affirmations-api-by-apirobots.p.rapidapi.com/v1/affirmations";
+  import.meta.env.VITE_SERVER_URL || 'http://localhost:3001'
