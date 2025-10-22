cdd88e441e248cc5150c4f92d23c319feec224f2
diagnosa <diagnosa@kth.se>
2025-10-22 13:05:29 +0200

    update mongo connection string

--- server/src/service/databaseService.ts
+++ server/src/service/databaseService.ts
@@ -4,13 +4,12 @@ const env = process.env?.NODE_ENV || 'development'
 
 const mongoDBUrl =
   env == 'production'
-    ? `mongodb+srv://<${process.env.MONGO_INITDB_ROOT_USERNAME}>:<${process.env.MONGO_INITDB_ROOT_PASSWORD}>@${process.env.MONGO_HOST}/?retryWrites=true&w=majority&appName=${process.env.MONGO_APP}`
+    ? `mongodb+srv://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.MONGO_HOST}/?retryWrites=true&w=majority&appName=${process.env.MONGO_APP}`
     : `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}?authSource=admin`
 
 // connect mongodb to server
 export const connectDB = async () => {
   try {
-    console.log('connection to mongo ' + mongoDBUrl)
     await mongoose.connect(mongoDBUrl)
   } catch (err) {
     console.error('MongoDB connection error:', err)
