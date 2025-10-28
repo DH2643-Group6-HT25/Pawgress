ec019baecd6844eba6a9b18c4a351f4141ae95eb
klarakarlssoon <klakar@kth.se>
2025-10-28 23:39:46 +0100

    change lax to none

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -37,7 +37,7 @@ router.post('/login', async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'lax',
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     })
 
@@ -76,7 +76,7 @@ router.post('/signup', async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'lax',
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     })
 
